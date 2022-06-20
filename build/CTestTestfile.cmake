2555d0001540e9e5a4232ac7c7e5ed3225d989d5
erkanoguz <ooguzerkan@gmail.com>
2022-06-20 20:09:59 +0300

    unnecessary files removed

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eoguz/Workspace/Erkan/Works/C++/my_github_repos/cpp-algorithm-design
-# Build directory: /home/eoguz/Workspace/Erkan/Works/C++/my_github_repos/cpp-algorithm-design/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
