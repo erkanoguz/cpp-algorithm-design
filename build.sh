621a2510b0030a48adb68bbf3ba368138b68166a
bbuyukyuksel <buyukyukselburak@gmail.com>
2022-06-24 00:20:41 +0300

    bug fix

--- build.sh
+++ build.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-BUILD_DIR="${PROJECT_DIR}/build/"
-CONAN_DIR="${PROJECT_DIR}/conan/"
-
-if [ -d "$BUILD_DIR" ]; then
-    cd $BUILD_DIR
-else
-    mkdir $BUILD_DIR
-    cd $BUILD_DIR
+BUILD_TYPE="Release"
+if [ "$#" -ge "1" ]; then
+        BUILD_TYPE=$1;
 fi
 
-conan install ${CONAN_DIR}
-cmake .. -DCMAKE_BUILD_TYPE=test 
-cmake --build .
+PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BUILD_DIR="${PROJECT_DIR}/build/"
+BIN_DIR="${PROJECT_DIR}/bin/"
+CONAN_DIR="${PROJECT_DIR}/conan/"
+
+rm -rf "$BUILD_DIR";
+rm -rf "$BIN_DIR";
+
+echo "Project dir: " ${PROJECT_DIR};
+echo "Build   dir: " ${BUILD_DIR};
+echo "Conan   dir: " ${CONAN_DIR};
+echo "BUILD  TYPE: " ${BUILD_TYPE}
+
+if [ -d "$BUILD_DIR" ]; then
+    cd "$BUILD_DIR"
+else
+    mkdir "$BUILD_DIR"
+    cd "$BUILD_DIR"
+fi
+
+conan install -s build_type=$BUILD_TYPE "${CONAN_DIR}"
+
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+cmake --build "$BUILD_DIR"
+
